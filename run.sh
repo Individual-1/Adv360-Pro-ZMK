d5acc182e21f72b635e6ce5c13640e54d8360c02
Indy <1389922+Individual-1@users.noreply.github.com>
2022-09-23 22:20:47 -0700

    Updating keymap

--- run.sh
+++ run.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-set -e
-
-docker run -it --name zmk zmk
-docker cp zmk:/app/firmware/ ./
+winpty docker run -it --name zmk zmk && docker cp zmk:/app/firmware/ ./
 docker stop zmk
 docker rm zmk
